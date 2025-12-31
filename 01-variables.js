16bbac85631fb255e2fbe82e5fba7ede4712191d
nurmaxamed73-crypto <nurmaxamed73@gmail.com>
2025-12-31 14:08:15 +0300

    Add variable declarations and console logs
    javascript variables

--- 01-variables.js
+++ 01-variables.js
@@ -1,1 +1,10 @@
+// JavaScript Variables
+
+let name = "Abdinor";
+let age = 20;
+let isLearning = true;
+
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("Learning JavaScript:", isLearning);
 
